41e917341916012a4b30c92d647d181c024c76d2
Isaac Ayodeji <deejecks@live.com>
2014-12-07 03:58:41 +0100

    further work

--- js/app.js
+++ js/app.js
@@ -7,11 +7,17 @@ var wordMatch = {
     this.word = $('input').val();
     console.log(this.word);
     this.definition();
+    this.thesaurus();
   },
 
   returnWord: function () {
     this.definitionUrl = "http://api.wordnik.com:80/v4/word.json/" + this.word + "/definitions/";
     return this.definitionUrl;
+  },
+
+  returnThesaurusUrl: function () {
+    this.thesaurusUrl = "http://words.bighugelabs.com/api/2/f5c79d25e5e8f8723ee62b695a8a7987/" + this.word + "/json";
+    return this.thesaurusUrl;
   },
 
   action: function () {
@@ -28,20 +34,55 @@ var wordMatch = {
   definitionCallback: function (response) {
     console.log(response);
     definitionHTML = '<ul>';
-    $.each(response, function (i, definition) {
+    $.each (response, function (i, definition) {
       definitionHTML += '<li>';
       definitionHTML += '<p>';
       definitionHTML += definition.text;
       definitionHTML += '</p></li>';
     });
     definitionHTML += '</ul>';
-    $('.definition').html(definitionHTML);
+    $('.def').html(definitionHTML);
+  },
+
+  thesaurusCallback: function (response) {
+    console.log(response);
+    thesaurusHTML = '<ul>';
+    $.each (response, function (i, partsOfSpeech) {
+      for (var syn in partsOfSpeech) {
+        if (partsOfSpeech.hasOwnProperty(syn)) {
+          console.log(partsOfSpeech.syn);
+          thesaurusHTML += '<li>';
+          thesaurusHTML += '<p>';
+          thesaurusHTML += '<span>';
+          thesaurusHTML += response.partsOfSpeech + ": ";
+          thesaurusHTML += '</span>';
+          for (var i = 0; i < partsOfSpeech.syn.length; i++) {
+            thesaurusHTML += partsOfSpeech.syn[i] + ", ";
+          }
+          thesaurusHTML += '</p></li>';
+        }
+      }
+
+      for (var ant in partsOfSpeech) {
+        if (partsOfSpeech.hasOwnProperty(ant)) {
+          console.log(partsOfSpeech.ant);
+        }
+      }
+    });
+    thesaurusHTML += '</ul>';
+    $('.thes').html(thesaurusHTML);
   },
 
   //getting word definition
   definition: function () {
     this.returnWord();
     $.getJSON(this.definitionUrl, this.definitionData, this.definitionCallback);
+  },
+
+  //getting thesaurus
+  thesaurus: function () {
+    this.returnThesaurusUrl();
+    $.getJSON(this.thesaurusUrl, this.thesaurusCallback);
   }
 };
 
